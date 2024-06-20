02bca2f62321536ff91b84c50470973e44f0d910
Alex Vydryakov <nt-vydryakov@mail.ru>
2024-06-20 22:08:54 +0300

    объединил всё

--- js/main.js
+++ js/main.js
@@ -1,29 +1,45 @@
 if(document.documentElement.clientWidth < 767) {
-  console.log('что то происходит')
 
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-  spaceBetween: 16,
-  slidesPerView: 1.3,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  //   type: 'bullets',
-  },
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 16,
+    slidesPerView: 1.3,
 
-  keyboard: {
-      enabled: true,
-      onlyInViewport: false,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    //   type: 'bullets',
     },
 
-  mousewheel: {
-  invert: true,
-  },
-});
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      },
 
+    mousewheel: {
+    invert: true,
+    },
+  });
 
+} else {
+  let buttonOpen = document.querySelector('.footer__open');
+  let buttonClose = document.querySelector('.footer__close');
+  let swiper = document.querySelector('.swiper-wrapper');
+
+  buttonOpen.addEventListener('click', function() {
+      swiper.classList.remove('swiper-wrapper--height');
+      buttonOpen.classList.add('hide');
+      buttonClose.classList.remove('hide');
+  })
+
+  buttonClose.addEventListener('click', function() {
+      swiper.classList.add('swiper-wrapper--height');
+      buttonOpen.classList.remove('hide');
+      buttonClose.classList.add('hide');
+  })
 }
+
