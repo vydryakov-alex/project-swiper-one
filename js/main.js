70d4b5fb3aceb0cc7163ac7c64bd18e2ed3f3b6b
Alex Vydryakov <nt-vydryakov@mail.ru>
2024-06-20 21:16:52 +0300

    третий до объединения

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
+if(document.documentElement.clientWidth < 767) {
+  console.log('что то происходит')
+
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    spaceBetween: 16,
-    slidesPerView: 1.3,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    //   type: 'bullets',
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 16,
+  slidesPerView: 1.3,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  //   type: 'bullets',
+  },
+
+  keyboard: {
+      enabled: true,
+      onlyInViewport: false,
     },
 
-    keyboard: {
-        enabled: true,
-        onlyInViewport: false,
-      },
+  mousewheel: {
+  invert: true,
+  },
+});
 
-    mousewheel: {
-    invert: true,
-    },
 
-    
-  
-
-  
-
-  });
+}
